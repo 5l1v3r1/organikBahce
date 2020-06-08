c2b1dbaebb3b98d4a92ba303bb83bc7d8caec45c
Emrah SEMİZ <emrahsemiz@hotmail.com>
2020-06-08 17:02:34 +0300

    Add project files.

---
+++ organikBahce.Entities/Conrete/Adress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace organikBahce.Entities.Conrete
+{
+    public class Adress
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string TcNumber { get; set; }
+        public string Phone { get; set; }
+        public string MobilePhone { get; set; }
+        public int ContryId { get; set; }
+        public int MyProperty { get; set; }
+        public string AdressName { get; set; }
+        public string Description { get; set; }
+        public DateTime createdDate { get; set; }
+        public DateTime modifiedDate { get; set; }
+        public bool isActive { get; set; }
+    }
+}
---
+++ organikBahce.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="organikBahce.WebUI.MvcApplication" Language="C#" %>
